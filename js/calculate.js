9916a9a9b0427d30d9d15e9723204f3c6d6cd6d0
Luis <hypnoticsiege@gmail.com>
2022-03-28 12:20:23 -0400

    Update calculate.js

--- js/calculate.js
+++ js/calculate.js
@@ -12,7 +12,5 @@ for (var i = 0; i < listItem.length; i++) {
     itemArray.push(parseInt(listItem[i].dataset.price));
 };
 
-console.table(itemArray);
-
 const sum = itemArray.reduce((partialSum, a) => partialSum + a, 0);
 document.getElementById("total").innerHTML = `Total: $${sum}`;
